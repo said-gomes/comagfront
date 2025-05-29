31a10a22f850447c162651556a207ac6084b8dee
Joey Alan <joeyalandev@gmail.com>
2025-05-29 02:35:56 -0300

    ###Changed

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -5,7 +5,7 @@ const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001';
 export const authService = {
   login: async (loginData: { email: string; password: string }) => {
     try {
-      const response = await axios.post(`${API_URL}/api/auth/login`, loginData, {
+      const response = await axios.post(`${API_URL}api/auth/login`, loginData, {
         headers: { 'Content-Type': 'application/json' }
       });
       console.log("Token recebido da API:", response.data.token); // <-- Adicione esta linha
